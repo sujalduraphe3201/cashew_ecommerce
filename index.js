144417e82d44e1c91fa17a44d49817c51dba81ba
sujalduraphe3201 <sujalduraphe3201@gmail.com>
2025-04-14 11:19:21 +0530

    12 push

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ app.use(cors({
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'client/dist')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+
 // Route for placing orders via Checkout (POST /orders)
 app.use('/orders', orderRoutes);
 // Route for fetching orders for the admin page (GET /admin/orders)
